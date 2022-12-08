60b0af4c1bbd7701f46bb888ba2dbbf9ebf93801
Hazel Windle <hwindle83@googlemail.com>
2022-12-08 22:11:31 +0000

    Completed JavaScript exercises

---
+++ main.js
@@ -0,0 +1,13 @@
+let age = 36;
+console.log(age);
+
+let numberOfRabbits = 2;
+numberOfRabbits *= 3;
+console.log(numberOfRabbits);
+
+let bodmasExample = 3;
+const result = (bodmasExample + 5) / 6;
+console.log(result);
+
+let randNum = Math.floor(Math.random() * 6 + 1);
+console.log(randNum);
