7d7553fc86b850536e3afd0852fc9d083ed24ced
Hazel Windle <hwindle83@googlemail.com>
2022-12-10 13:32:27 +0000

    Added messages to HTML page

--- 02_conditionals/main.js
+++ 02_conditionals/main.js
@@ -26,24 +26,35 @@ if (height >= 150 && typeof(height) === 'number') {
   even number: one free throw
   other combination: loose game
 */
+function showResult(inputText) {
+  const resultElement = document.querySelector('#result');
+  resultElement.innerHTML = inputText;
+}
+
 
 let die1 = Math.floor(Math.random() * 6 + 1);
 // and dice throw two
 let die2 = Math.floor(Math.random() * 6 + 1);
 let isTotalEven = (die1 + die2) % 2 === 0;
+// text string to show user
+let result = '';
 // if both are six win top prize
 if (die1 === 6 && die2 === 6) {
-  alert(`You get top prize as you rolled ${die1} and ${die2}.\n
-    You win a bear!`);
+  result = `You get top prize as you rolled ${die1} and ${die2}.\n
+    <strong><em>You win a bear!</em></strong>`;
+  showResult(result);
 } else if (die1 === die2) {
   // if both die equal one another, 3 free throws
-  console.log(`You have two equal dice, ${die1} and ${die2}.\n
-    You get 3 extra throws!`);
+  result = `You have two equal dice, ${die1} and ${die2}.\n
+    You get 3 extra throws!`;
+  showResult(result);
 } else if (isTotalEven) {
   // if the total dice roll is even: one free throw (variable)
-  console.log(`You have an even total, from ${die1} and ${die2}.\n
-    You get an extra throw.`);
+  result = `You have an even total, from ${die1} and ${die2}.\n
+    You get an extra throw.`;
+  showResult(result);
 } else {
   // else for other combination.
-  console.log(`You got ${die1} and ${die2}. You loose. :-(`);
+  result = `You got ${die1} and ${die2}. You loose. :-(`;
+  showResult(result);
 }
